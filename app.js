656921e7ab6687cbd16ece8d3f4ebf6cfa3b0df7
chin14 <s.dj14@icloud.com>
2021-07-02 13:08:23 +0200

    get size of image

--- app.js
+++ app.js
@@ -7,6 +7,29 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'view')));
 
+//first promise
+const imageSize = new Promise( (resolve, reject) =>{
+  if('jpg'){
+    const sizeOf = require('image-size');
+    //Get size of every image
+    const dimensionsFrau = sizeOf('www/images/frau.jpg');
+    const dimensionsReh = sizeOf('www/images/reh.jpg');
+    const dimensionsSchwarzweiß = sizeOf('www/images/schwarzweiß.jpg');
+    const dimensionsPferd = sizeOf('www/images/pferd.jpg');
+    //put it out
+    console.log(JSON.stringify(dimensionsFrau.width, dimensionsFrau.height));
+    console.log(JSON.stringify(dimensionsReh.width, dimensionsReh.height));
+    console.log(JSON.stringify(dimensionsSchwarzweiß.width, dimensionsSchwarzweiß.height));
+    console.log(JSON.stringify(dimensionsPferd.width, dimensionsPferd.height));
+
+
+
+    resolve(sizeOf)
+  }else{  
+    const reason = new Error('something went wrong UPS');
+    reject(reason);
+  }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
