8fe9837181de5b83cc42fa3f4a1b0408dd281a3c
chin14 <s.dj14@icloud.com>
2021-07-26 21:16:42 +0200

    image information
    get change-date, height and width from image

--- app.js
+++ app.js
@@ -3,44 +3,98 @@ const app = express();
 const fs = require('fs');
 const port = 5000;
 const path = require('path');
+const imageSize = require('image-size');
+const { resolve } = require('path');
 
 
+app.use('/images', express.static('www/images'));
 app.use(express.static(path.join(__dirname, 'view')));
 
 //first promise
-const imageSize = new Promise( (resolve, reject) =>{
+const allSize = new Promise( (resolve, reject) =>{
   if('jpg'){
-    const sizeOf = require('image-size');
+    const imageSize = require('image-size');
     //Get size of every image
-    const dimensionsFrau = sizeOf('www/images/frau.jpg');
-    const dimensionsReh = sizeOf('www/images/reh.jpg');
-    const dimensionsSchwarzweiß = sizeOf('www/images/schwarzweiß.jpg');
-    const dimensionsPferd = sizeOf('www/images/pferd.jpg');
+    const dimensionsFrau = imageSize('www/images/frau.jpg');
+    const dimensionsReh = imageSize('www/images/reh.jpg');
+    const dimensionsSchwarzweiß = imageSize('www/images/schwarzweiß.jpg');
+    const dimensionsPferd = imageSize('www/images/pferd.jpg');
     //put it out
-    console.log(JSON.stringify(dimensionsFrau.width, dimensionsFrau.height));
-    console.log(JSON.stringify(dimensionsReh.width, dimensionsReh.height));
-    console.log(JSON.stringify(dimensionsSchwarzweiß.width, dimensionsSchwarzweiß.height));
-    console.log(JSON.stringify(dimensionsPferd.width, dimensionsPferd.height));
+    console.log(dimensionsFrau.width, dimensionsFrau.height);
+    console.log(dimensionsReh.width, dimensionsReh.height);
+    console.log(dimensionsSchwarzweiß.width, dimensionsSchwarzweiß.height);
+    console.log(dimensionsPferd.width, dimensionsPferd.height);
 
 
 
-    resolve(sizeOf)
+    resolve(imageSize)
   }else{  
     const reason = new Error('something went wrong UPS');
     reject(reason);
   }
 });
 
-fs.stat('./www/images/', (err, stats) => {
-  if(err) {
-      throw err;
-  }
 
-  // print file last modified date
-  console.log(`File Data Last Modified: ${stats.mtime}`);
-  console.log(`File Status Last Modified: ${stats.ctime}`);
+const Datainformation = new Promise((resolve, reject) =>{
+  const fileName = path.join(__dirname, './www/images');
+  fs.readdir(fileName, (err, files) =>{
+    if(err){
+      console.log('unable to get file' + err);
+    } else{
+      const Array = [];
+      files.forEach((files)=>{
+        if(files !== '.DS_Store'){
+          return console.log(Array.push({name: files, imageSize: imageSize('www/images/' + files)})); 
+        }
+      })
+        console.log(Array);
+        //Neues Promise 
+      
+    }
+ });
+})
+
+
+
+
+ const getTimeChange = new Promise((resolve, reject)=>{
+ const getFile = path.join(__dirname, './www/images');
+ if(err){
+   console.log('Ups Something went wrong');
+ }else{
+  
+   fs.stat(getFile, (err, file) =>{
+     if(err){
+        console.log(err);
+     }else{
+       const ArrayOne = [];
+       file.forEach((file)=>{
+       if(file === 'jpg'){
+        return console.log(ArrayOne.push(`File Data Last Modified: ${stats.mtime} ${stats.ctime}` ));
+      }
+    })
+    console.log(ArrayOne);
+     }
+   })
+
+ }
 });
+
+// fs.stat('./www/images/', (err, stats) => {
+//   if(err) {
+//       throw err;
+//   }
+
+//   // print file last modified date
+//   console.log(`File Data Last Modified: ${stats.mtime}`);
+//   console.log(`File Status Last Modified: ${stats.ctime}`);
+// });
+Promise.all([allSize, Datainformation, getTimeChange])
+ .then(result => console.log(result))
+ .catch(error => console.log(`Error in promises ${error}`))
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   });
+
+module.export = {allSize, Datainformation, getTimeChange};
---
+++ view/main.js
@@ -0,0 +1,47 @@
+//import {imageSize} from app.js
+
+const btn = document.getElementById('btn')
+btn.onclick = () => {
+    // this.imageSize = imageSize();
+
+    for (let i = 1; i < 5; i++){
+      let td = document.createElement('td');
+
+      let text1 = document.createTextNode((' '));
+}
+// const btn = document.getElementById('btn')
+// btn.onclick = () => 
+// { const table = document.createElement('table');
+// for (let i = 1; i < 5; i++){
+//     let tr = document.createElement('tr');   
+
+//     let td1 = document.createElement('td');
+//     let td2 = document.createElement('td');
+//     let td3 = document.createElement('td');
+//     let td4 = document.createElement('td');
+
+
+//     let text1 = document.createTextNode('Text1');
+//     let text2 = document.createTextNode('Text2');
+//     let text3 = document.createTextNode('Text3');
+//     let text4 = document.createTextNode('Text4');
+
+
+//     td1.appendChild(text1);
+//     td2.appendChild(text2);
+//     td3.appendChild(text3);
+//     td4.appendChild(text4);
+
+//     tr.appendChild(td1);
+//     tr.appendChild(td2);
+//     tr.appendChild(td3);
+//     tr.appendChild(td4);
+
+
+//     table.appendChild(tr);
+// }
+// document.body.appendChild(table);
+//  };
+
+
+}
