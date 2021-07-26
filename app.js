f911c74d161d795ec52f1e7d1529727955873dd4
chin14 <s.dj14@icloud.com>
2021-07-26 21:19:44 +0200

    update code dit

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 const port = 5000;
 const path = require('path');
 const imageSize = require('image-size');
-const { resolve } = require('path');
-
 
 app.use('/images', express.static('www/images'));
 app.use(express.static(path.join(__dirname, 'view')));
@@ -35,8 +33,24 @@ const allSize = new Promise( (resolve, reject) =>{
 });
 
 
+const getTimeChange = new Promise((resolve, reject) =>{
+  const getFile = path.join(__dirname, './www/images');
+  if(!getFile){
+    console.log('Ups Something went wrong');
+  }else{
+    for (let i = 0; i < getFile.length; i++) {
+      const stats = fs.stat(getFile[i]);
+      if (stats !== '.DS_Store') {
+          console.log(stats.mtime + stats.ctime)
+      }
+  }
+  }
+ });
+ 
+
 const Datainformation = new Promise((resolve, reject) =>{
   const fileName = path.join(__dirname, './www/images');
+
   fs.readdir(fileName, (err, files) =>{
     if(err){
       console.log('unable to get file' + err);
@@ -44,7 +58,7 @@ const Datainformation = new Promise((resolve, reject) =>{
       const Array = [];
       files.forEach((files)=>{
         if(files !== '.DS_Store'){
-          return console.log(Array.push({name: files, imageSize: imageSize('www/images/' + files)})); 
+          return  console.log(Array.push({name: files, imageSize: imageSize('www/images/' + files), getTimeChange: stats.ctime(path.join(__dirname, './www/images'))})); 
         }
       })
         console.log(Array);
@@ -52,43 +66,13 @@ const Datainformation = new Promise((resolve, reject) =>{
       
     }
  });
+    
 })
 
 
 
 
- const getTimeChange = new Promise((resolve, reject)=>{
- const getFile = path.join(__dirname, './www/images');
- if(err){
-   console.log('Ups Something went wrong');
- }else{
-  
-   fs.stat(getFile, (err, file) =>{
-     if(err){
-        console.log(err);
-     }else{
-       const ArrayOne = [];
-       file.forEach((file)=>{
-       if(file === 'jpg'){
-        return console.log(ArrayOne.push(`File Data Last Modified: ${stats.mtime} ${stats.ctime}` ));
-      }
-    })
-    console.log(ArrayOne);
-     }
-   })
 
- }
-});
-
-// fs.stat('./www/images/', (err, stats) => {
-//   if(err) {
-//       throw err;
-//   }
-
-//   // print file last modified date
-//   console.log(`File Data Last Modified: ${stats.mtime}`);
-//   console.log(`File Status Last Modified: ${stats.ctime}`);
-// });
 Promise.all([allSize, Datainformation, getTimeChange])
  .then(result => console.log(result))
  .catch(error => console.log(`Error in promises ${error}`))
@@ -96,5 +80,3 @@ Promise.all([allSize, Datainformation, getTimeChange])
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   });
-
-module.export = {allSize, Datainformation, getTimeChange};
